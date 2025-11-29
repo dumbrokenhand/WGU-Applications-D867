5e4773ec627b427a0a72d1b17c97ebfc461d72a7
dumbrokenhand <jackcollins7794@gmail.com>
2025-11-29 15:26:52 -0800

    Refactor Student class with proper constructors and methods

--- student.h
+++ student.h
@@ -4,7 +4,7 @@ using namespace std;
 
 class Student {
     private:
-        // E. variables for Student Class
+        // 1.  Create the class Student in the files student.h and student.cpp, which includes each of the following private variables, using the correct data type for each:
         string studentID;
         string firstName;
         string lastName;
@@ -39,8 +39,8 @@ class Student {
             this->degreeProgram = degreeProgram;
         }
 
-        // print function
-        void Student::print() {
+        // d.  a print() function to print specific student data in the provided format:
+        void PrintFunction() {
             cout << studentID << "\t"; 
             cout << "First Name: " << firstName << "\t";
             cout << "Last Name: " << lastName << "\t";
@@ -62,55 +62,55 @@ class Student {
         // 2a. getters
         string getStudentID() const {
             return studentID;
-        };
+        }
         string getFirstName() const {
             return firstName;
-        };
+        }
         string getLastName() const {
             return lastName;
-        };
+        }
         string getEmailAddress() const {
             return emailAddress;
-        };
+        }
         int getAge() const {
             return age;
-        };
+        }
         int getDaysInCourse0() const {
             return daysInCourse[0];
-        };
+        }
         int getDaysInCourse1() const {
             return daysInCourse[1];
-        };
+        }
         int getDaysInCourse2() const {
             return daysInCourse[2];
-        };
-        string getDegreeProgram() const {
+        }
+        DegreeProgram getDegreeProgram() const {
             return degreeProgram;
-        };
+        }
 
         // 2b. setters
-        void setStudentID() {
-            this->studentID = studentID;
-        };
-        void setFirstName() {
-            this->firstName = firstName;
-        };
-        void setLastName() {
-            this->lastName = lastName;
-        };
-        void setEmailAddress() {
-            this->emailAddress = emailAddress;
-        };
-        void setAge() {
-            this->age = age;
-        };
-        void setDaysInCourse() {
-            this->daysInCourse[0] = daysInCourse[0];
-            this->daysInCourse[1] = daysInCourse[1];
-            this->daysInCourse[2] = daysInCourse[2];
-        };
-        void setDegreeProgram() {
+        void setStudentID(string newStudentID) {
+            studentID = newStudentID;
+        }
+        void setFirstName(string newFirstName) {
+            firstName = newFirstName;
+        }
+        void setLastName(string newLastName) {
+            lastName = newLastName;
+        }
+        void setEmailAddress(string newEmailAddress) {
+            emailAddress = newEmailAddress;
+        }
+        void setAge(int newAge) {
+            age = newAge;
+        }
+        void setDaysInCourse(array <int,3> newDaysInCourse) {
+            daysInCourse[0] = newDaysInCourse[0];
+            daysInCourse[1] = newDaysInCourse[1];
+            daysInCourse[2] = newDaysInCourse[2];
+        }
+        void setDegreeProgram() { // fix this function to refelct proper parameter
             this->degreeProgram = degreeProgram;
-        };
+        }
 
 };
