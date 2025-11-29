9dd625c97009d656fc3b4d0af2858cb3712d3e1c
dumbrokenhand <jackcollins7794@gmail.com>
2025-11-29 15:27:16 -0800

    Create Roster class with classRosterArray

---
+++ roster.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include "student.h"
+using namespace std;
+
+
+// F.  Create a Roster class (roster.cpp) by doing the following:
+class Roster {
+    //1.  Create an array or vector of pointers named classRosterArray to hold the data provided in the "studentData Table."
+    public:
+        string classRosterArray[6] = {
+            
+        }:
+
+    private:
+}
